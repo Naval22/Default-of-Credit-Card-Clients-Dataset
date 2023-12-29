1f946ad2d3450f3af98d3d68a15d3cada7a0c8f4
Revision-at-340805875063 <jangidnaval9@gmail.com>
2023-12-29 13:30:21 +0530

    modifie exception.py

--- src/exception.py
+++ src/exception.py
@@ -20,13 +20,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message    
-    
-
-if __name__ =="__main__":
-    logging.info("logging is created")
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("error")
-        raise CustomException(e,sys)       
